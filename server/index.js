754019023a9e4dcc181d01285b4521cb080b5e1b
Nguyễn Duy Nguyễn <20110530@student.hcmute.edu.vn>
2023-04-22 19:50:17 +0700

    Lấy tất cả xe show lên trang chủ

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import xe from './router/xe.js'
 import danhmuc from './router/danhmuc.js'
 import hangxe from './router/hangxe.js'
 import _AuthMiddleWare from './common/_AuthMiddleWare.js';
@@ -18,6 +19,7 @@ app.use(cors());
 
 app.use('/', danhmuc);
 app.use('/', hangxe);
+app.use('/', xe);
 app.use('/', signIn);
 app.use('/', signUp);
 app.use(_AuthMiddleWare.isAuth);
