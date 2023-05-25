aa9bd06de676c14bbdea8f729fe15cdaedb1feab
Nguyễn Duy Nguyễn <20110530@student.hcmute.edu.vn>
2023-05-25 14:44:38 +0700

    Singleton

--- server/common/connect.js
+++ server/common/connect.js
@@ -1,14 +1,37 @@
 import mysql from 'mysql';
 
-var connection = mysql.createConnection({
-  host: "brcqbyhqtad5pdyqakqn-mysql.services.clever-cloud.com",
-  user: "u6qqbf6dz9fzwj2z",
-  password: "biGVAjyJ1GR4IhNpFeb9",
-  database: "brcqbyhqtad5pdyqakqn"
-});
+class DatabaseConnection {
+  constructor() {
+    this.connection = null;
+  }
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-export default connection;
+  connect() {
+    this.connection = mysql.createConnection({
+      host: "brcqbyhqtad5pdyqakqn-mysql.services.clever-cloud.com",
+      user: "u6qqbf6dz9fzwj2z",
+      password: "biGVAjyJ1GR4IhNpFeb9",
+      database: "brcqbyhqtad5pdyqakqn"
+    });
+
+    this.connection.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+  }
+
+  getConnection() {
+    if (!this.connection) {
+      this.connect();
+    }
+    return this.connection;
+  }
+
+  static getInstance() {
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+    }
+    return DatabaseConnection.instance;
+  }
+}
+
+export default DatabaseConnection.getInstance().getConnection();
